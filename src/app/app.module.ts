9b9db3cebdc88a3e7a5b4f3f8b2f86753419c7ba
Elina <elluksm@gmail.com>
2020-07-29 23:11:59 +0300

    add heading component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { SubscriptionFormComponent } from './subscription-form/subscription-form.component';
+import { HeadingComponent } from './heading/heading.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ToolbarComponent,
-    FooterComponent
+    FooterComponent,
+    SubscriptionFormComponent,
+    HeadingComponent
   ],
   imports: [
     BrowserModule
